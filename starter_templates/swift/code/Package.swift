6553c7fc36cae395ab38d5eb9eb89de68321e7cf
Shon Frazier <code@shon.io>
2024-07-21 22:33:46 -0400

    use SwiftNIO

--- starter_templates/swift/code/Package.swift
+++ starter_templates/swift/code/Package.swift
@@ -5,10 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "codecrafters-build-http-server-swift",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio", from: "2.42.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "codecrafters-build-http-server-swift"),
+            name: "codecrafters-build-http-server-swift",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ]
+        ),
     ]
 )
